913649bdaf4738d17ac0d720603e73f4ec1e54ef
churles <ocana.churleslester@gmail.com>
2021-11-26 16:51:53 +0800

    minor changes

--- booksys/settings.py
+++ booksys/settings.py
@@ -81,6 +81,16 @@ TEMPLATES = [
 WSGI_APPLICATION = 'booksys.wsgi.application'
 
 ASGI_APPLICATION = 'booksys.routing.application'
+
+CHANNEL_LAYERS = {
+    'default':{
+        'BACKEND':'channels_redis.core.RedisChannelLayer',
+        'CONFIG':{
+            'hosts':['127.0.0.1','8000'],
+        },
+    },
+}
+
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
