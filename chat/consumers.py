215c07e418999809cdc5647afbcca19833bb1e2c
churles <ocana.churleslester@gmail.com>
2021-11-27 14:59:01 +0800

    add major changes to chat app

--- chat/consumers.py
+++ chat/consumers.py
@@ -1,5 +1,5 @@
-from channels.generic.websocket import AsyncWebsocketConsumer
 import json
+from channels.generic.websocket import AsyncWebsocketConsumer
 
 class ChatRoomConsumer(AsyncWebsocketConsumer):
 	async def connect(self):
