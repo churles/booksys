c69b49a4866ecb2f81805955e75b8efd1c62102d
Churles Ocana <ocana.churleslester@gmail.com>
2021-12-09 15:08:38 +0800

    added book_library

--- books/forms.py
+++ books/forms.py
@@ -10,7 +10,6 @@ class CreateBook(forms.ModelForm):
 			'title': forms.TextInput(attrs={'class': 'form-control'}),
 			'author': forms.TextInput(attrs={'class': 'form-control'}),
 			'description': forms.Textarea(attrs={'class': 'form-control'}),
-			# 'genre': forms.Select(attrs={'class': 'form-control'}, choices=GENRE_CHOICES),
 			'slug': forms.HiddenInput(),
 			'thumbnail': forms.FileInput(attrs={'class': 'form-control-file'}),
 			'condition': forms.Select(attrs={'class': 'form-control'}),
